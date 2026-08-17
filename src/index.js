da21d490c812bf3add0f478966d11f51bfea14a8
Arjun Shrirang Gondhale <gondhalearjun5@gmail.com>
2026-08-17 16:52:39 +0530

    Utils code write and write error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config()
-connectDB()
+connectDB().then(() => {
+    const port = process.env.PORT || 8000
+    app.on("error", (error) => {
+        console.log("ERROR :", error);
+        throw error
+
+    })
+    app.listen(port, () => {
+        console.log(`App is listen From Port ${port} `)
+    })
+
+}).catch((err) => {
+    console.log("MONGODB connection Faile ...!! ", err)
+
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+    return (res, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+export { asyncHandler }
+
+
+// const asyncHandler = (fn) => {
+//     async (req, res, next) => {
+//         try {
+//             await fn(req, res, next)
+//             next()
+
+//         } catch (error) {
+//             res.status(error.code || 500).json({
+//                 success: false,
+//                 message: error.message
+//             })
+
+//         }
+
+//     }
+// }
