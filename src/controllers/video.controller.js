81d132e9f04afcfbb72fa9b88e695827f4ba651e
Arjun Shrirang Gondhale <gondhalearjun5@gmail.com>
2026-09-03 19:59:04 +0530

    feat: enhance getVideoById function with validation and error handling

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -147,7 +147,30 @@ const publishAVideo = asyncHandler(async (req, res) => {
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
-    //TODO: get video by id
+
+    if (!videoId) {
+        throw new ApiError(400, "Video Id Is Missing ")
+    }
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Video Id")
+    }
+
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(404, "Video dosent Exist")
+    }
+
+    if (!video.isPublished) {
+        if (!video.owner.equels(req.user?._id)) {
+            throw new ApiError(404, " Video not found ")
+
+        }
+    }
+
+    return res.status(200)
+        .json(
+            new ApiResponse(200, { video }, "Video Found Successfully")
+        )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
